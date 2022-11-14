6de05de3d619be9f18ffabdc15662686c96b5693
russbus11 <russell@shipt.com>
2022-11-14 11:32:08 -0600

    I created a main.go file and finished my Testing project

---
+++ testcalculator.go
@@ -0,0 +1,22 @@
+package test calculator
+
+import "errors"
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil
+}
